cd98b9b215a238d39540c662ddec4e03aae71cd9
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-12-03 13:01:15 -0500

    onchange calls api for every letter pressed - bug

--- src/SearchParameter.jsx
+++ src/SearchParameter.jsx
@@ -1,39 +1,42 @@
 import React, { useState, useEffect } from "react";
-import pet, { ANIMALS } from "@frontendmasters/pet";
+// import pet, { ANIMALS } from "@frontendmasters/pet";
 import { useDropdown } from "./hooks/use-dropdown";
+import { API_KEY_CMC } from "../config_keys";
 
 const SearchParams = () => {
-    //const location = "Seattle, WA";
+    // const cryptocurrency = "Bitcoin";
 
-    const [location, setLocation] = useState("Seattle, WA");
-    const [breeds, setBreeds] = useState([]); //breeds will update
+    const [cryptocurrency, setCryptocurrency] = useState("Bitcoin"); //rerenders
+    // const [breeds, setBreeds] = useState([]); //breeds will update
     // const [animal, setAnimal] = useState("dog");
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    // const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
     // const [breed, setBreed] = useState("");
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+    // const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
     //Schedules this effect to run. render ui first
     useEffect(() => {
-        pet.breeds("dogs").then(console.log, console.error);
+        //pet.breeds("dogs").then(console.log, console.error);
+        const url = `https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY_CMC}`;
+        fetch(url).then(console.log, console.error);
     });
 
     return (
         <div className='search-params'>
-            <h1>{location}</h1>
+            <h1>{cryptocurrency}</h1>
             <form>
-                <label htmlFor='location'>
-                    Location
+                <label htmlFor='cryptocurrency'>
+                    cryptocurrency
                     <input
-                        id='location'
-                        value={location}
+                        id='cryptocurrency'
+                        value={cryptocurrency}
                         onChange={event => {
-                            setLocation(event.target.value);
+                            setCryptocurrency(event.target.value); //rerenders and pulls api every letter!!!!
                         }}
-                        placeholder='Location'
+                        placeholder='cryptocurrency'
                     />
                 </label>
-                <AnimalDropdown />
-                <BreedDropdown />
+                {/* <AnimalDropdown /> */}
+                {/* <BreedDropdown /> */}
                 <button>Submit</button>
             </form>
         </div>

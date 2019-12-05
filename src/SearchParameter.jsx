1f5c757335fb1f273f2792ee02ec63486f6e1495
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2019-12-05 09:59:57 -0500

    project set up for bungie api

--- src/SearchParameter.jsx
+++ src/SearchParameter.jsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 // import pet, { ANIMALS } from "@frontendmasters/pet";
 import { useDropdown } from "./hooks/use-dropdown";
-import { list, info } from "./endpoints/endpoints";
+// import { list, info } from "./endpoints/endpoints";
+import { bungie, fetchOptions } from "./endpoints/endpoints";
 
-// const list = "data/listings.json";
-//const info = "data/info.json";
+const list = "./data/listings.json";
+const info = "./data/info.json";
 
 const SearchParams = () => {
     // const cryptocurrency = "Bitcoin";
 
     const [cryptocurrency, setCryptocurrency] = useState("Bitcoin"); //rerenders
     const [coins, setCoins] = useState({});
+    // const [bungie, setBungie] = useState({});
+
+    useEffect(() => {
+        fetch(bungie, fetchOptions)
+            .then(response => response.json())
+            .then(destiny => console.log(destiny))
+            .catch(err => console.error(err));
+    }, []);
+
     // const [breeds, setBreeds] = useState([]); //breeds will update
     // const [animal, setAnimal] = useState("dog");
     // const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
@@ -18,18 +28,49 @@ const SearchParams = () => {
     // const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
     //Schedules this effect to run. render ui first
-    useEffect(() => {
-        //pet.breeds("dogs").then(console.log, console.error);
-        //const url = `https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY_CMC}`;
+    // useEffect(() => {
+    //     //pet.breeds("dogs").then(console.log, console.error);
 
-        // fetch(list).then(console.log, console.error);
-        fetch(list)
-            .then(res => res.json())
-            .then(data => console.log("data", data))
-            .catch(err => console.log("error: ", err));
-    });
+    //     // fetch(list).then(console.log, console.error);
+    //     fetch(list, {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json"
+    //         }
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setCoins(data.data);
+    //         })
+    //         .catch(err => console.log("error: ", err));
+    // }, []); // make sure to put the empty array as the second arg
 
-    // const [coinDropdown, setcoinDropdown] = useState();
+    const [coin, CoinDropdown] = useDropdown(
+        "CryptoCurrency",
+        "Choose Coin",
+        coins
+    );
+
+    //WES BOS
+    console.log("about to 🐕");
+    const wes = fetch("https://wesbos.com/wp-json/wp/v2/posts");
+
+    wes.then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error("error!!!: ", err));
+
+    console.log("done!");
+
+    //creating a promise
+    const p = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(Error("Wes is so not cool"));
+        }, 1000);
+    }); //object
+
+    p.then(data => console.log("my promise: ", data)).catch(err =>
+        console.error("my promise error: ", err)
+    );
 
     return (
         <div className='search-params'>
@@ -47,9 +88,12 @@ const SearchParams = () => {
                         placeholder='cryptocurrency'
                     />
                 </label>
-                {coins.name}
-                {/* <CoinCategoryDropdown /> */}
-                {/* <BreedDropdown /> */}
+                {coins.length > 0 ? (
+                    "coin drop" // <CoinDropdown />
+                ) : (
+                    <div className='loader'>Loading...</div>
+                )}
+
                 <button>Submit</button>
             </form>
         </div>
--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,4 +1,4 @@
-import { API_KEY_CMC } from "../../config_keys";
+import { API_KEY_CMC, API_Key_BUNGIE } from "../../config_keys";
 
 const cors = "https://cors-anywhere.herokuapp.com/";
 const baseUrl = cors + "https://pro-api.coinmarketcap.com/v1";
@@ -6,3 +6,38 @@ const id = 2;
 
 export const list = `${baseUrl}/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY_CMC}`; //all coins, overview
 export const info = `${baseUrl}/cryptocurrency/info?id=${id}&CMC_PRO_API_KEY=${API_KEY_CMC}`; //single description, details, category
+
+//Bungie
+export const fetchOptions = {
+    headers: {
+        "X-API-Key": API_Key_BUNGIE
+    }
+};
+const base = cors + "https://www.bungie.net/Platform";
+export const bungie = `${base}/App/FirstParty/`;
+// export const map =
+//     baseUrl + "/cryptocurrency/map?CMC_PRO_API_KEY=" + API_KEY_CMC; //start stop for coin
+// export const quote =
+//     baseUrl +
+//     "/cryptocurrency/quotes/latest?id=" +
+//     id +
+//     "&CMC_PRO_API_KEY=" +
+//     API_KEY_CMC; //single price action, overview
+// export const fcasList =
+//     baseUrl +
+//     "partners/flipside-crypto/fcas/listings/latest?CMC_PRO_API_KEY=" +
+//     API_KEY_CMC; //scores, grade
+// export const fcasQuote =
+//     baseUrl +
+//     "partners/flipside-crypto/fcas/quotes/latest?id=" +
+//     id +
+//     "&CMC_PRO_API_KEY=" +
+//     API_KEY_CMC; //single score, grade
+// export const metrics =
+//     baseUrl + "/global-metrics/quotes?CMC_PRO_API_KEY=" + API_KEY_CMC;
+
+// https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyMap
+
+// others
+// http://hn.algolia.com/api/v1/
+// https://wesbos.com/wp-json //https://wesbos.com/wp-json/wp/v2/posts
